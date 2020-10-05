919faf4de182fa3a649dff4bb12c94abbd808202
brunamarques <bruna99stefany@gmail.com>
2020-10-05 20:22:59 -0300

    tentando carregar arquivos

--- AG.py
+++ AG.py
@@ -1,9 +1,15 @@
 import csv
 dic = {}
+data = {}
 
 with open('teste.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
+    n = 0
     for row in reader:
-        dic = {'teste':(row['Low'], row['High'])}
+        n+=1
+        data[row] = {}
+       # print(row['Low'], row['High'])
+        for line in reader:
+             data[row][line[0]] = line[n]
 
-    print(dic)
+print(data)    

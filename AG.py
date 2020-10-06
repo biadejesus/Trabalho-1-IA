f823250cbe719d72d06c07f89f7136d25f68207c
biadejesus <beatriz.jesus.costa@gmail.com>
2020-10-06 10:38:53 -0300

    funçao de ler arquivo

--- AG.py
+++ AG.py
@@ -1,11 +1,30 @@
 import csv
+import numpy as np
 dic = {}
-data = {}
 
-with open('teste.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    result = []
-    for row in reader:
-         result.append(dict(row))
-    print(result[0]['Low'])
-    print(result[0]['High'])
+arquivos = ['LREN3.SA.csv', 'teste.csv']
+
+for i in range(2):
+    with open(arquivos[i], newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        result = []
+        for row in reader:
+            if row['Date'][0:4] != '2016':
+                result.append(dict(row))
+        dic[arquivos[i]] = result
+print(dic['teste.csv'][10])
+
+# 100 - (100/1+ U/D)
+
+
+# def calcula_rsi(dic):
+#     for 
+
+
+def popinicial (n):
+    matriz_pop = np.random.multinomial(100, np.ones(10)/10, size=n)
+    matriz_pop = [list(x) for x in matriz_pop]
+    print(matriz_pop)
+
+popinicial(10)
+
